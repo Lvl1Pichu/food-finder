87ffd63545d9d14e526904c0a76f60daad353f84
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-02-22 16:06:42 +0100

    Ready to merge main.

--- src/Api-test.tsx
+++ src/Api-test.tsx
@@ -1,44 +1,43 @@
 import { useEffect, useState } from "react";
 
-interface recipeCard {
-
+interface Recipe {
+  title: string;
 }
 
 function APITest() {
+  const [recipe, setRecipe] = useState("");
 
-
-  const [recipe, setRecipe] = useState('');
-
-  useEffect(() =>{
+  useEffect(() => {
     const options = {
       // ea186f9a58784d0d86b47956204c76be
-      method: 'GET',
+      method: "GET",
       headers: {
-        "Content-Type": "application/json"
-      }
-    }
+        "Content-Type": "application/json",
+      },
+    };
     async function getRecipe() {
-      const response = await fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=ea186f9a58784d0d86b47956204c76be&ingredients=apples,+flour,+sugar&number=1", options)
-      .then((r) => 
-        r.json()
+      const response = await fetch(
+        "https://api.spoonacular.com/recipes/findByIngredients?apiKey=ea186f9a58784d0d86b47956204c76be&ingredients=apples,+flour,+sugar&number=1",
+        options
       )
-      .then((data) =>
-        console.log(data)
-      );
+        .then((r) => r.json())
+        .then((r) => setRecipe(r.title))
+        .then((data) => console.log(data));
     }
-    if(!recipe) {
+    if (!recipe) {
       getRecipe();
     }
-  })
+  });
 
   return (
     <div>
       <h1>API Test</h1>
       <div className="card">
-        <p>
-          {recipe ? recipe : "Search for recipes"};
-        </p>
+        <p>{recipe ? recipe : "Search for recipes"};</p>
       </div>
     </div>
-  )
+  );
 }
+
+
+export default APITest;
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
-import { useEffect, useState } from 'react';
+import APITest from './Api-test';
 import './App.css';
 
 function App() {
 
-  const [recipe, setRecipe] = useState('');
-
-  useEffect(() =>{
-    const options = {
-      // ea186f9a58784d0d86b47956204c76be
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-    async function getRecipe() {
-      const response = await fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=ea186f9a58784d0d86b47956204c76be&ingredients=apples,+flour,+sugar&number=1", options)
-      .then((r) => 
-        r.json()
-      )
-      .then((data) =>
-        console.log(data)
-      );
-    }
-    if(!recipe) {
-      getRecipe();
-    }
-  })
-
-  return (
-    <div className="App">
-      <h1>API Test</h1>
-      <div className="card">
-        <p>
-          
-        </p>
-      </div>
-    </div>
+  return(
+    <APITest></APITest>
   )
 }
 
