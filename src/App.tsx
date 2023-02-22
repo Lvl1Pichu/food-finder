d551e3b9f6a5b31a53ac4563135e211ab4831348
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-02-22 15:06:38 +0100

    added header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import HeaderComponent from './HeaderComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <HeaderComponent></HeaderComponent>
     </div>
   )
 }
