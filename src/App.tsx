4a8a73ed31b8805b49ab9dfcc77b814276aac985
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-02-22 15:39:54 +0100

    Successful call to spoonacular.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [recipe, setRecipe] = useState('');
+
+  useEffect(() =>{
+    const options = {
+      // ea186f9a58784d0d86b47956204c76be
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    async function getRecipe() {
+      const response = await fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=ea186f9a58784d0d86b47956204c76be&ingredients=apples,+flour,+sugar&number=1", options)
+      .then((r) => 
+        r.json()
+      )
+      .then((data) =>
+        console.log(data)
+      );
+    }
+    if(!recipe) {
+      getRecipe();
+    }
+  })
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>API Test</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
