588316f2222b54b5aeec089218152be96bf42f24
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-02-23 17:31:13 +0100

    tested code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import ButtonComponent from './ButtonComponent'
-import HeaderComponent from './HeaderComponent'
+import StartPage from './pages/StartPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <HeaderComponent></HeaderComponent>
-      <ButtonComponent label="Click" onClick={() => console.log("Button clicked")} />
+      <StartPage></StartPage>
+      {/* <HeaderComponent></HeaderComponent>
+      <ButtonComponent label="Click" onClick={() => console.log("Button clicked")} /> */}
     </div>
   )
 }
