f5c5a2044ea1cb8eee2037a892dde6404dc1aad1
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-02-24 15:09:55 +0100

    Listing recipe titles.

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -7,13 +7,7 @@ function ResultPage() {
     const {ings} = useParams();
     console.log(ings);
     const [ingredients, setIngredients] = useState(ings);
-    const [recipes, setRecipes] = useState('');
-
-    function listIngredients() {
-        let ingsList
-        ingsList = ings?.split("+");
-        return ingsList?.map(ing => <li>{ing}</li>)
-    }
+    const [recipes, setRecipes] = useState<object[]>();
 
     useEffect(() => {
         console.log("UseEffect ran.")
@@ -25,18 +19,34 @@ function ResultPage() {
             },
           }
 
-        fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=ea186f9a58784d0d86b47956204c76be&ingredients=${ingredients}&number=1`,
+        fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=ea186f9a58784d0d86b47956204c76be&ingredients=${ingredients}&number=3`,
         options)
         .then((r) => r.json())
-        .then((data) => console.log(data));
-    }, //[ingredients]
+        .then((data) => setRecipes(data));
+    }, [ingredients] 
     )
+
+    function listIngredients() {
+        let ingsList
+        ingsList = ings?.replace("-", " ").split("+");
+        return ingsList?.map(ing => <li key={ing}>{ing}</li>) // Question! Can I remove question marks?
+    }
+
+    function listRecipes() {
+        if (recipes)
+       return recipes.map((r) => {return <li key={r.title}>{r.title}</li>})
+    }
 
     return(
         <main>
             <div>
                 <ul>
                     {listIngredients()}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {recipes ? listRecipes() : null}
                 </ul>
             </div>
         </main>
