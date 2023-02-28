d102ae291ba0c89f1f3db8bd1a6e5c20f7819aa3
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-02-28 14:45:40 +0100

    add recipe route

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import RecipePage from './pages/RecipePage'
 import ResultPage from './pages/ResultPage'
 import StartPage from './pages/StartPage'
 
@@ -9,8 +10,9 @@ const router =  createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       
-      <Route path="result/:ings" element={<ResultPage />}></Route> 
       <Route path="" element={<StartPage/>} />
+      <Route path="result/:ings" element={<ResultPage />}></Route> 
+      <Route path="recipe" element={<RecipePage />}></Route> 
       <Route path="*" element={<h2>404 not found</h2>} />
 
     </Route>
